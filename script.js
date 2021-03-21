733745c7390b1b03b82067ef00311f184dc94710
coder873 <74519793+coder873@users.noreply.github.com>
2021-03-20 21:31:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 (function (window) {
+ // ******************************* 
+// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
 // *******************************
-// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
-// *******************************
-//
+
 // Module 4 Assignment Instructions.
 //
 // The idea of this assignment is to take an existing array of names
@@ -64,5 +64,5 @@ for (var i in names) {
 }
 })(window);
 
-  
-
+ 
+/
